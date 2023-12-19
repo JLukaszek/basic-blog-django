f0bcd4b2274c98f047728a28a934656f9a62bb25
JLukaszek <jarek8331@gmail.com>
2023-12-19 23:43:55 +0100

    Added crispy forms to tempaltes.

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
     # 3rd party apps
     'bootstrap5',
+    'crispy_forms',
+    'crispy_bootstrap5',
 
     # My apps
     'blog.apps.BlogConfig',
@@ -134,6 +136,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = 'blog:home'
 LOGOUT_REDIRECT_URL = 'blog:home'
